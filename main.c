11b0e445c152373d96bbaf6227338c91f2a710b2
TheAstartes <HelioniX91@gmail.com>
2020-10-04 12:55:14 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
+
+		return 1;
+	}
+
+	char *p = argv[1];
+
+	printf(" .global main\n");
+	printf("main:\n");
+
+	printf(" mov $%ld, %%rax\n", strtol(p, &p, 10));
+
+	while(*p)
+	{
+			if (*p == '+')
+			{
+					printf(" add $%ld, %%rax\n", strtol(p, &p, 10));
+					continue;
+			}
+
+			if (*p == '-')
+			{
+					printf(" sub $%ld, %%rax\n", strtol(p, &p, 10));
+					continue;
+			}
+	}
+
+	fprintf(stderr, "unexpected character: '%c'\n", *p);
+
+	printf(" ret\n");
+
+	return 0;
+
+}
+
